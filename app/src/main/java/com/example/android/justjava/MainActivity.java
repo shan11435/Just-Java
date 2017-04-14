0e30980f8613df421ce486a4dea12c4dde983ffb
Tahsin Ahmed <shanahmed1997@gmail.com>
2017-04-14 15:48:52 -0400

    finished coffee app

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -26,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+       String priceMessage = "Total: $" + price + ".00" ;
+        priceMessage = priceMessage + "\nThank you";
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -56,5 +58,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
